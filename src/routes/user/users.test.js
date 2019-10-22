36f61c41106bc31a107ed02a62fcbba2e66dc5a6
WebCheng <webster0808web@gmail.com>
2019-10-21 18:20:58 -0700

    Mark testing case

--- src/routes/user/users.test.js
+++ src/routes/user/users.test.js
@@ -13,30 +13,24 @@ describe('Test /users', () => {
 })
 
 describe('Test users sign up and login success', () => {
-  let postUserData = {
-    email: 'corvallis@oregonstate.edu',
-    password: 'portland',
-    firstName: 'Test',
-    lastName: 'Hi',
-  }
-  test('User sign up', function(done) {
-    request(app)
-      .post('/users/signup')
-      .send(postUserData)
-      .set('Accept', 'application/json')
-      .then(response => {
-        expect(response.statusCode).toBe(409)
-        done()
-      })
-  })
-  test('User login', function(done) {
-    request(app)
-      .post('/users/login')
-      .send(postUserData)
-      .set('Accept', 'application/json')
-      .then(response => {
-        expect(response.statusCode).toBe(200)
-        done()
-      })
-  })
+  // let postUserData = {
+  //   email: 'corvallis@oregonstate.edu',
+  //   password: 'portland',
+  //   firstName: 'Test',
+  //   lastName: 'Hi',
+  // }
+  // let request = null
+  // test('User sign up', function(done) {
+  //   postUser()
+  // })
+  // test('User login', function(done) {
+  //   request(app)
+  //     .post('/users/login')
+  //     .send(postUserData)
+  //     .set('Accept', 'application/json')
+  //     .then(response => {
+  //       expect(response.statusCode).toBe(200)
+  //       done()
+  //     })
+  // })
 })
